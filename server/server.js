39665c9ee9060d7461c1e2f4e82ee88770b16b03
arifsiddiqui295 <arifsiddiqui2905@gmail.com>
2024-04-07 18:27:49 +0530

    inittial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const connectDB= require('./connection/connection.js');
+const userRoutes= require('./routes/index.js');
+const cors = require('cors');
+const app = express();
+const morgan = require('morgan');
+require('dotenv').config();
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/',userRoutes);
+connectDB();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
